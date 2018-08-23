46eaab87888234369ed062d15c29c8f80a22e443
Andrea Bagnacani <andrea.bagnacani@uni-rostock.de>
2018-08-23 10:51:00 +0200

    removed option ordered sort

--- src/thermo/concentrations/ReadCommandLine.c
+++ src/thermo/concentrations/ReadCommandLine.c
@@ -10,64 +10,6 @@
   is if prefix.con.  Uses the package getopt.h to retrieve option
   flags.
 
-  The command line option flags are
-  -sort [required argument]
-     sort = 0: the output is not sorted, but listed in the
-       order in which it was inputted.
-     sort = 1 (default for method = 1): the output is sorted first by 
-       permutation concentration.
-     sort = 2: the output is sorted first by complex ID number and then
-       by permutation ID number.
-     sort = 3: the output is sorted first by complex concentration.  The 
-       permutation concentrations within each complex are then sorted.
-     sort = 4: the output is sorted first by size of complexes and then 
-       "alphabetically" within each complex size.  I.e., say there are 
-       two strands, A, and B.  The ordering for complex size 2 is AA, AB, 
-       BB.  The permutations are sorted within each complex but permutation
-       ID number.
-  -T [required argument] 
-     The temperature, in degrees C, at which the calculation will be
-     done.  Note that the partition functions used in the calculation
-     should be calculated at the same temperature.  The default is T =
-     37.
-  -quiet [no argument]
-     Selecting this flag will supress output to the screen.
-  -tol [required argument]
-     The tolerance for converging to the equilibrium concentrations.
-     This is entered as a fraction of the input concentration of the 
-     single-stranded species.  I.e., if we have two strands A and B 
-     that form complexes, and the input concentration of A is 1e-6 
-     molar and B is 2e-6 molar, a value of tol = 0.001 means that 
-     the maximal error in the conservation of mass for strand A is 
-     0.001*1e-6 molar and 0.001*2e-6 for strand B.  The default value 
-     is tol = 0.0000001.
-  -maxtrial [required argument]
-     The maximum number of initial conditions to try before stopping.
-     The default is maxtrial = 100.  Not relevant for method = 2.
-  -maxnostep [required argument]
-     The maximum number of iterations in the trust region optimization
-     that may be tried without a step being taken.  This corresponds
-     to a very small trust region due to precision issues.  If
-     maxnostep is exceeded, a new initial condition is tried.  The
-     default is maxnostep = 50.  Not relevant for method = 2.
-  -perturbscale [required argument]
-     When new initial conditions are generated, they are perturbed
-     from the standard initial condition by a prefactor multiplied
-     by a random number from -1 to 1.  This prefactor is perturbscale.
-     Its value is adjusted in the program in order to make sure there
-     are no overflow errors.  The default is perturbscale = 100.  Not
-     relevant for method = 2.
-  -writelogfile [no argument]
-     When this flag is selected, a log file is written which contains
-     information about the trust region steps and convergence.  Default
-     is not to write a log file.
-  -seed [required argument]
-     Give a seed to the random number generator
-  -help [no argument]
-     Prints the help file to the screen.
-  -validate [no argument]
-     Prints all floats to 14 decimal places
-
   Justin Bois, Caltech, 2 September 2006
 */
 
@@ -84,7 +26,6 @@ void ReadCommandLine(int nargs, char **args, char *cxFile, char *conFile,
 
   int options;  // Counters used in getting flags
   int ShowHelp; // ShowHelp = 1 if help option flag is selected
-  // int NoSortOutputOption; // No sorting option given from command line
   char prefix[MAXLINE]; // The prefix for the input and output files
   char InputStr[MAXLINE]; // Dummy string for storing flag options from command line
   FILE *fp; // The cx file, used to check if we can open it.
@@ -95,7 +36,6 @@ void ReadCommandLine(int nargs, char **args, char *cxFile, char *conFile,
     exit(ERR_NOINPUT);
   }
 
-  // NoSortOutputOption = 1; // No sorting option given from command line
   *SortOutput = 1;  // Default is to sort the output by concentration
   *MaxIters = 10000; // Default maxiters
   *tol = 0.0000001; // Default tolerance is 0.00001% of minimum of the
@@ -120,7 +60,6 @@ void ReadCommandLine(int nargs, char **args, char *cxFile, char *conFile,
     {
       static struct option long_options [] =
 	      {
-          {"sort",          required_argument,  0, 'a'},
 	        {"tol",           required_argument,  0, 'c'},
 	        {"T",             required_argument,  0, 'd'},
 	        {"quiet",         no_argument,        0, 'e'},
@@ -129,7 +68,6 @@ void ReadCommandLine(int nargs, char **args, char *cxFile, char *conFile,
 	        {"help",          no_argument,        0, 'h'},
 	        {"perturbscale",  required_argument,  0, 'i'},
 	        {"writelogfile",  no_argument,        0, 'j'},
-	        {"ordered",       no_argument,        0, 'k'},
 	        {"seed",          required_argument,  0, 'm'},
           {"validate",      no_argument,        0, 'o'},
           {0, 0, 0, 0}
@@ -139,7 +77,7 @@ void ReadCommandLine(int nargs, char **args, char *cxFile, char *conFile,
 
 
       options = getopt_long_only (nargs, args, 
-				  "a:b:c:d:ef:g:hi:jklm:o", long_options, 
+				  "c:d:ef:g:hi:jm:o", long_options, 
 				  &option_index);
 
       // Detect the end of the options.
@@ -148,12 +86,6 @@ void ReadCommandLine(int nargs, char **args, char *cxFile, char *conFile,
 
       switch (options)
         {
-        case 'a':
-	        strcpy(InputStr,optarg);
-	        *SortOutput = atoi(InputStr);
-	        // NoSortOutputOption = 0; // Record that we've selected a sorting option
-          break;
-
 	      case 'c':
 	        strcpy(InputStr,optarg);
 	        *tol = str2double(InputStr);
@@ -273,31 +205,8 @@ void DisplayHelpConc() {
   printf("Usage: concentrations [OPTIONS] PREFIX\n");
   printf("Calculate concentrations for each complex specified\n");
   printf("Options:\n");
-  printf(" -ordered             perform the calculation on ordered complexes\n");
-  printf(" -sort METHOD         change the sort method for the .eq output file\n");
-  printf("                      0: same as input\n");
-  printf("                      1: sort by concentration of complex; if -ordered\n");
-  printf("                         specified, then sort by concentration of ordered\n");
-  printf("                         complex\n");
-  printf("                      2: sort by concentration of complex and then, if\n");
-  printf("                         if -ordered is selected, by the concentration\n");
-  printf("                         each constituent ordered complex\n");
-  printf("                      3: output is sorted by complex/ordered complex id\n");
-  printf("                      4: output is sorted by strands in the complex,\n");
-  printf("                         then by strand quantities, then by ordered\n");
-  printf("                         complex identifier\n");
   printf(" -quiet               suppress output to the screen\n");
   printf("\n");
 
 }
-/* ******************************************************************************** */
-void print_deprecation_info(FILE *out) {
-  char *dep_mess = 
-  "Relative to NUPACK 3.0, the following change was introduced to the\n"
-  "concentrations executable: the -ordered option is on by default.\n";
 
-  fprintf(out, "%s", dep_mess);
-  
-  return;
-}
-/* ******************************************************************************** */
