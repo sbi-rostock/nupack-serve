b5dc64b01ba1ea98b0d46dfb94b5793fae2e6895
Andrea Bagnacani <andrea.bagnacani@uni-rostock.de>
2018-09-07 00:14:43 +0200

    removed input filename constraint

--- src/thermo/concentrations/ReadCommandLine.c
+++ src/thermo/concentrations/ReadCommandLine.c
@@ -14,15 +14,9 @@ void ReadCommandLine(int nargs, char **args, int *SortOutput, int *MaxIters,
         unsigned long *seed, int *NUPACK_VALIDATE){
 
   int options;
-  int ShowHelp; // 1 if help option flag is selected
-  char prefix[MAXLINE]; // prefix for the input and output files
-  char InputStr[MAXLINE]; // dummy string for storing flag options from command line
+  int option_index = 0;
+  char InputStr[MAXLINE];
 
-  if (nargs == 1) {
-    printf("For instructions on running this program, run it with the ");
-    printf("-help flag.\n\nExiting....\n\n");
-    exit(ERR_NOINPUT);
-  }
 
   *SortOutput = 1;  // Default is to sort the output by concentration
   *MaxIters = 10000; // Default maxiters
@@ -37,23 +31,19 @@ void ReadCommandLine(int nargs, char **args, int *SortOutput, int *MaxIters,
   *seed = 0; // Default is to seed off the clock.
   double cutoff = 0.001; // Default cutoff
   *NUPACK_VALIDATE = 0;
-  ShowHelp = 0;
 
   SetExecutionPath(nargs, args);
 
-  // Get the option flags
+  // get the option flags
   while (1){
     static struct option long_options[] = {
         {"T",             required_argument,  0, 'd'},
-        {"help",          no_argument,        0, 'h'},
         {"validate",      no_argument,        0, 'o'},
         {0, 0, 0, 0}
     };
-    /* getopt_long stores the option index here. */
-    int option_index = 0;
 
 
-    options = getopt_long_only (nargs, args, "d:ho", long_options,
+    options = getopt_long_only (nargs, args, "d:o", long_options,
         &option_index);
 
     // detect the end of the options
@@ -63,13 +53,9 @@ void ReadCommandLine(int nargs, char **args, int *SortOutput, int *MaxIters,
 
     switch(options){
       case 'd':
-        strcpy(InputStr,optarg);
-        *kT = kB*(str2double(InputStr) + ZERO_C_IN_KELVIN);
+        strcpy(InputStr, optarg);
+        *kT = kB * (str2double(InputStr) + ZERO_C_IN_KELVIN);
         *Toverride = 1;
-        break;
-
-      case 'h':
-        ShowHelp = 1;
         break;
 
       case 'o':
@@ -84,32 +70,8 @@ void ReadCommandLine(int nargs, char **args, int *SortOutput, int *MaxIters,
     }
   }
 
-  if(ShowHelp){
-    DisplayHelpConc();
-    exit(ERR_HELP);
-  }
-
   if(*SortOutput > 4){
     *SortOutput = 1;
   }
-
-  // Get the the input file
-  if(optind == nargs){ // no input from the user
-    exit(ERR_NOINPUT);
-  }
-  else{
-    strcpy(prefix,args[optind]);
-  }
-}
-
-
-
-/* Displays the contents of the Concentrations help file
- */
-void DisplayHelpConc(){
-
-  printf("Please read the NUPACK User Guide for detailed instructions.\n");
-  printf("Usage: concentrations [OPTIONS] PREFIX\n");
-
 }
 
